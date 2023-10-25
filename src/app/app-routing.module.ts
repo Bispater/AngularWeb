7c656d77b8903c0fa7013cbecffe8e5087cea392
Felipe Gallardo <felipe.gallardo.estay@gmail.com>
2023-10-25 12:09:13 -0300

    contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Define a route for the home component
+  { path: 'contacto', component: ContactComponent}
 ];
 
 @NgModule({
